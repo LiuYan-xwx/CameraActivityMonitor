62e4a2c60114f9f6ecb05e397d9c6c6447ace268
LiuYan-xwx <LiuYan_xwx@outlook.com>
2026-05-07 15:59:42 +0800

    fix: 修复converter签名

--- CameraActivityMonitor/Converters/BooleanToIsUseStatusConverter.cs
+++ CameraActivityMonitor/Converters/BooleanToIsUseStatusConverter.cs
@@ -4,7 +4,7 @@ namespace CameraActivityMonitor.Converters
 {
     public class BooleanToIsUseStatusConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
         {
             if (value is bool b)
             {
@@ -12,7 +12,7 @@ namespace CameraActivityMonitor.Converters
             }
             return "未知";
         }
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotImplementedException();
         }
