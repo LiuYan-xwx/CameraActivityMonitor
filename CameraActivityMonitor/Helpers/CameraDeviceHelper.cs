ec300c2c6177c1b472b2100b975ff52a6789d959
LiuYan-xwx <LiuYan_xwx@outlook.com>
2026-06-01 12:06:57 +0800

    Revert "feat: 引入摄像头预览功能并集成 OpenCvSharp4"
    This reverts commit 09fea8837b85359cda6f43d654ed44ffc06e5aec.

--- CameraActivityMonitor/Helpers/CameraDeviceHelper.cs
+++ CameraActivityMonitor/Helpers/CameraDeviceHelper.cs
@@ -15,19 +15,4 @@ public static class CameraDeviceHelper
         var devices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
         return devices.Count > 0 ? devices[0].Id : null;
     }
-
-    public static async Task<int?> TryGetCameraIndexByIdAsync(string cameraId)
-    {
-        var devices = await GetAllCamerasAsync();
-        int index = -1;
-        for (var i = 0; i < devices.Count; i++)
-        {
-            if (devices[i].Id == cameraId)
-            {
-                index = i;
-                break;
-            }
-        }
-        return index >= 0 ? index : null;
-    }
 }
